49aae36ca1d8bd8198be8a74b92931c8445fd7f6
Zachary Dionne <zachary.dionne.01@edu.cegeptr.qc.ca>
2023-03-14 17:31:21 -0400

    Algorithme en cours...

--- Assets/Rubiks_Cube/RubiksCube.cs
+++ Assets/Rubiks_Cube/RubiksCube.cs
@@ -855,7 +855,7 @@ public class RubiksCube : MonoBehaviour
 			face1 = GetFaceFromColor(objectivePattern, pattern[FindCuby(pattern, wantedCuby)][0].ToString());
 			face2 = GetFaceFromColor(objectivePattern, pattern[FindCuby(pattern, wantedCuby)][1].ToString());
 
-Debug.Log($"-----------------{cubyRef}-------------");Print<string>(pattern);Debug.Log("on tourne la face oppose");
+Debug.Log($"-----------------{pattern[FindCuby(pattern, wantedCuby)]}-------------");Print<string>(pattern);Debug.Log("on tourne la face oppose");
 
 			//tourne la face de couleur oppose pour que le cubyRef (cuby ayant la place ou on veut aller) soit en bas
 			directionSide1 = RotateWith(pattern, orderList, Array.IndexOf(RubikData.indexFaceMap, face1), directionSide1, () => {
@@ -868,13 +868,13 @@ Debug.Log("tourne le bas afin que le cubyRef soit sur la face oppose");
 			directionDown = RotateWith(pattern, orderList, 5, directionDown, () => {
 				return IsCubyOn(face2, FindCuby(pattern, cubyRef));
 			});
-
+Print<string>(pattern);Debug.Log("remonte");
 			//remonte
 			Rotate(pattern, orderList, Array.IndexOf(RubikData.indexFaceMap, face1), -directionSide1);
-			
+Print<string>(pattern);Debug.Log("replace le bas");	
 			//replace le bas
 			Rotate(pattern, orderList, Array.IndexOf(RubikData.indexFaceMap, face2), -directionDown);
-			
+Print<string>(pattern);
 
 
 
